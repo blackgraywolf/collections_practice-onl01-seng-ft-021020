1d82c4b64a522c3f0721da088aaeefbd423ae267
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-16 02:40:00 +0000

    Automatically backed up by Learn

--- spec/collections_practice_spec.rb
+++ spec/collections_practice_spec.rb
@@ -1,69 +1,99 @@
-require 'spec_helper'
 
-describe 'collections practice' do
+#QUESTION1
+def sort_array_asc(array1)
+  array1.sort
+end
+#array1 = [25, 7, 1]
+#sort_array_asc(array1)
 
-  # Question 1
-  describe '#sort_array_asc' do
-    it 'should return an array sorted in ascending order' do
-      expect(sort_array_asc([25, 7, 1])).to eq([1,7,25])
-    end
-  end
+#QUESTION2
+def sort_array_desc(array2)
+  array2.sort do |a,b| 
+    b <=> a
+    end 
+end
 
-  # Question 2
-  describe '#sort_array_desc' do
-    it 'should return an array sorted in descending order' do
-      expect(sort_array_desc([25, 7, 14])).to eq([25, 14, 7])
-    end
-  end
+#array2 = [2,72,5,25, 7, 1]
+#sort_array_desc(array2)
 
-  # Question 3
-  describe '#sort_array_char_count' do
-    it 'should return an array in ascending order sorted by the number of characters in the string' do
-      expect(sort_array_char_count(["dogs", "cat", "Horses"])).to eq(["cat", "dogs", "Horses"])
-    end
-  end
-
-  # Question 4
-  describe '#swap_elements' do
-    it 'swap the second and third elements of an array' do
-      expect(swap_elements(["blake", "ashley", "scott"])).to eq(["blake", "scott", "ashley"])
-    end
-  end
-
-  # Question 5
-  describe '#reverse_array' do
-    it 'reverse the order of an array of integers' do
-      expect(reverse_array([12, 4, 35])).to eq([35, 4, 12])
-    end
-  end
-
-  # Question 6
-  describe '#kesha_maker' do
-    # DO NOT USE .collect or .map. use .each to build a new array, like in the "my_each" lab!
-    it 'taking an array as an input, change the 3rd character of each element to a dollar sign.' do
-      expect(kesha_maker(["blake", "ashley", "scott"])).to eq(["bl$ke", "as$ley", "sc$tt"])
-    end
-  end
-
-  # Question 7
-  describe '#find_a' do
-    it 'find all words that begin with "a" in the following array' do
-      expect(find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ])).to eq(["apple", "avis", "arlo", "ascot"])
-    end
-  end
-
-  # Question 8
-  describe '#sum_array' do
-    #for a bonus, look up the `.inject` method to get this test passing
-    it 'sum all the numbers in the following array' do
-      expect(sum_array([11,4,7,8,9,100,134])).to eq(273)
-    end
-  end
-
-  # Question 9
-  describe '#add_s' do
-    it 'Add an "s" to each word in the array except for the 2nd element in the array' do
-      expect(add_s(["hand","feet", "knee", "table"])).to eq(["hands","feet", "knees", "tables"])
-    end
+#QUESTION3
+def sort_array_char_count(array3)
+  array3.sort do |a, b|
+    a.length <=> b.length
   end
 end
+#array3=["dogs", "cat", "Horses"]
+#sort_array_char_count(array3)
+
+#QUESTION4
+
+def swap_elements(array4)
+  array4[1],array4[2] = array4[2],array4[1]
+  array4
+end
+
+#def swap_elements_from_to(array4,index,destination_index)
+#   swap1 = array4[index]
+#   swap2 = array4[destination_index]
+#   array4[index] = swap2
+#   array4[destination_index] = swap1 
+#   array4
+#end
+#array4 = [2,72,5,25]
+#swap_elements_from_to(array3,0,2)
+
+#QUESTION5
+def reverse_array(array5)
+  array5.reverse
+end
+#array5 = [2,72,5,25]
+#reverse_array(array5)
+
+
+#QUESTION6
+def kesha_maker(array6)
+  array6.each {|x| x[2] = "$"}   
+end
+
+#array6 = ["blake", "ashley", "scott"]
+#kesha_maker(array6)
+
+#QUESTION7
+def find_a(array7)
+  array7 = array7.select {|n| n[0].include?("a")==true} 
+end
+#array7 = ["apple", "orange", "pear", "avis", "arlo", "ascot" ]
+#find_a(array7)
+
+#QUESTION8
+
+def sum_array(array8)
+  array8.inject {|n,array8| n+array8}
+end  
+#array8=[11,4,7,8,9,100,134]
+#sum_array(array8)
+
+#QUESTION 9
+
+def add_s(array9) 
+  array9.collect do |element| 
+  if element != "feet" 
+    element+"s"
+  else 
+    element           
+  end
+  end 
+end 
+
+#def add_s(array8)   
+#  array9.each_with_index.collect{|element, index| "#{element} #{index}"
+#  if "#{index}" != "1"
+#    element+"s"
+#  else
+#    element  
+#  end
+#  }
+#end
+
+#array9 = ["hand","feet", "knee", "table"]
+#add_s(array9)
